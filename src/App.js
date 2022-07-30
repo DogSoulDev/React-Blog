b82d0e64d705a9f9acfc577cef95b3de262e9485
Javier Fernández <dogsouldev@gmail.com>
2022-07-30 12:39:53 +0200

    add login with google and redirect to home

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
-
+import { useState } from 'react';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   return (
 <Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/createpost">Create Post</Link>
+    <Link to="/login">Login</Link>
+
+  </nav>
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/createpost" element={<CreatePost />} />
-    <Route path="/login" element={<Login />} />
+    <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
   </Routes>
   </Router>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
+function Login({ isAuth, setIsAuth }) {
+  const navigate = useNavigate();
+	const signInWithGoogle = () => {
+		signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true)
+			setIsAuth(true);
+      navigate("/")
+		});
+	};
+	return (
+		<div className='loginPage'>
+			<p>Sign In With Google to Continue</p>
+			<button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with Google</button>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
